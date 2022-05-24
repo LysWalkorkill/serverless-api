96498d1133c425e3c0d79aa11661c1c5e98c84b1
hleonardtek <hadrien.leonard@epitech.eu>
2022-05-24 18:15:35 +0200

    add writeMessage endpoint OK

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
-
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 admin.initializeApp();
 
 const auth = admin.auth();
+const db = getFirestore();
+
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
@@ -24,4 +26,19 @@ exports.getAllUsers = functions.https.onRequest((req, res) => {
     .then(() => {
         res.send(JSON.stringify(tmp))
     })
-  });
+});
+
+exports.writeMessage = functions.https.onRequest((req, res) => {
+    const docRef = db.collection('messages').doc();
+
+    docRef.set({
+        message: req.body.message,
+        from: req.body.ownerID,
+        to: req.body.receiverID,
+        timestamp: Date.now()
+        }).catch(err => {
+            console.log(err)
+        }).then(() => {
+            res.send("Message collection updated !")
+        });
+});
