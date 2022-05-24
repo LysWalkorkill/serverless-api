fefd53e0067a848b163bbe9cfa5a74ed20b7ea2a
hleonardtek <hadrien.leonard@epitech.eu>
2022-05-24 17:50:19 +0200

    add getAllusers endpoint to fetch users

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,9 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
 
+admin.initializeApp();
+
+const auth = admin.auth();
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
@@ -7,3 +11,17 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+exports.getAllUsers = functions.https.onRequest((req, res) => {
+    let tmp = [];
+  
+    auth.listUsers().then((userRecords) => {
+      userRecords.users.forEach((user) => {
+          tmp.push({id: user.uid, email: user.email})
+        //   console.log(tmp)
+        });
+    }).catch((error) => console.log(error))
+    .then(() => {
+        res.send(JSON.stringify(tmp))
+    })
+  });
