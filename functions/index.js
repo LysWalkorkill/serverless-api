430052621d07c7bff6d6c626377e5566292cbb7e
hleonardtek <hadrien.leonard@epitech.eu>
2022-05-24 18:58:50 +0200

    lint index.js

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require("firebase-functions");
-const admin = require('firebase-admin');
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const admin = require("firebase-admin");
+const {getFirestore} = require("firebase-admin/firestore");
 admin.initializeApp();
 
 const auth = admin.auth();
@@ -15,30 +15,33 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 });
 
 exports.getAllUsers = functions.https.onRequest((req, res) => {
-    let tmp = [];
-  
-    auth.listUsers().then((userRecords) => {
-      userRecords.users.forEach((user) => {
-          tmp.push({id: user.uid, email: user.email})
-        //   console.log(tmp)
-        });
-    }).catch((error) => console.log(error))
-    .then(() => {
-        res.send(JSON.stringify(tmp))
-    })
+  const tmp = [];
+
+  auth.listUsers().then((userRecords) => {
+    userRecords.users.forEach((user) => {
+      tmp.push({id: user.uid, email: user.email});
+      //   console.log(tmp)
+    });
+  }).catch((error) => {
+    functions.logger.error(error, {structuredData: true});
+    console.log(error);
+  }).then(() => {
+    res.send(JSON.stringify(tmp));
+  });
 });
 
 exports.writeMessage = functions.https.onRequest((req, res) => {
-    const docRef = db.collection('messages').doc();
+  const docRef = db.collection("messages").doc();
 
-    docRef.set({
-        message: req.body.message,
-        from: req.body.ownerID,
-        to: req.body.receiverID,
-        timestamp: Date.now()
-        }).catch(err => {
-            console.log(err)
-        }).then(() => {
-            res.send("Message collection updated !")
-        });
+  docRef.set({
+    message: req.body.message,
+    from: req.body.ownerID,
+    to: req.body.receiverID,
+    timestamp: Date.now(),
+  }).catch((error) => {
+    functions.logger.error(error, {structuredData: true});
+    console.log(error);
+  }).then(() => {
+    res.send("Message collection updated !");
+  });
 });
